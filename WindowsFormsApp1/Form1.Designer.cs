971e217c318948d38de20c9d64b3243c35e6139b
svennn-7836 <orvillemallari@gmail.com>
2022-04-30 16:28:50 +0800

    Include Button Function

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class MessageBoxCreation
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessageBoxCreation));
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -36,27 +36,27 @@
             // 
             this.button1.AllowDrop = true;
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.AutoEllipsis = true;
             this.button1.AutoSize = true;
             this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button1.Font = new System.Drawing.Font("Bradley Hand ITC", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(84, 135);
+            this.button1.Location = new System.Drawing.Point(87, 125);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(255, 29);
+            this.button1.Size = new System.Drawing.Size(247, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "SVEN ORVILLE D. MALLARI";
+            this.button1.Text = "Click here to show Full Name";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // MessageBoxCreation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(446, 293);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "MessageBoxCreation";
             this.Text = "ASSIGNMENT#1 - \"MESSAGEBOX\".exe";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,16 +10,16 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class MessageBoxCreation : Form
     {
-        public Form1()
+        public MessageBoxCreation()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            MessageBox.Show("SVEN ORVILLE D. MALLARI");
         }
     }
 }
